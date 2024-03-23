2753c7de9f4499b5b37d81049d373037b65f8e14
charma1k <128212291+charma1k@users.noreply.github.com>
2024-03-23 11:36:53 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,122 +1,94 @@
 # Вариант 15
 # Формируется матрица F следующим образом: если в Е количество чисел, больших К в четных столбцах в области 1 больше, 
 # чем сумма чисел в нечетных строках в области 3, то поменять в Е симметрично области 1 и 3 местами, иначе В и С поменять местами несимметрично. 
-# При этом матрица А не меняется. После чего вычисляется выражение: AF– KAT . 
+# При этом матрица А не меняется. После чего вычисляется выражение: AF– KA^T . 
 # Выводятся по мере формирования А, F и все матричные операции последовательно.
-
 
 import random
 
-
-def generate_matrix(N):
+def generate_matrix(size):
     matrix = []
-    for _ in range(N):
+    for _ in range(size):
         row = []
-        for _ in range(N):
+        for _ in range(size):
             row.append(random.randint(-10, 10))
         matrix.append(row)
     return matrix
 
-
 def print_matrix(matrix):
     for row in matrix:
-        print(row)
+        print(" ".join(str(elem) for elem in row))
 
+def submatrix(matrix, row_start, row_end, col_start, col_end):
+    return [row[col_start:col_end] for row in matrix[row_start:row_end]]
 
-def get_submatrix(matrix, start_row, start_col, size):
-    submatrix = []
-    for i in range(start_row, start_row + size):
-        submatrix.append(matrix[i][start_col:start_col + size])
-    return submatrix
+def check_condition(submatrix, k_value):
+    even_columns_sum = 0
+    for col in range(len(submatrix[0])):
+        for row in range(len(submatrix)):
+            if submatrix[row][col] > k_value:
+                even_columns_sum += 1
 
+    odd_rows_sum = 0
+    for row in range(1, len(submatrix), 2):
+        for col in range(len(submatrix[row])):
+            odd_rows_sum += submatrix[row][col]
 
-def count_greater_than_K(matrix, K):
-    count = 0
-    for row in matrix:
-        for num in row:
-            if num > K:
-                count += 1
-    return count
+    return even_columns_sum > odd_rows_sum
 
+def swap_submatrices(matrix, sub1, sub2):
+    for i in range(len(sub1)):
+        for j in range(len(sub1[i])):
+            matrix[sub1[i][j]], matrix[sub2[i][j]] = matrix[sub2[i][j]], matrix[sub1[i][j]]
 
-def swap_submatrices(matrix, submatrix1, submatrix2, start_row, start_col, size):
-    for i in range(size):
-        for j in range(size):
-            matrix[start_row + i][start_col + j], matrix[submatrix1[0] + i][submatrix1[1] + j] = \
-                matrix[submatrix1[0] + i][submatrix1[1] + j], matrix[start_row + i][start_col + j]
-            matrix[start_row + i][start_col + j], matrix[submatrix2[0] + i][submatrix2[1] + j] = \
-                matrix[submatrix2[0] + i][submatrix2[1] + j], matrix[start_row + i][start_col + j]
-
-
-def calculate_F(matrix, K):
-    E = get_submatrix(matrix, 1, 1, 2)
-    B = get_submatrix(matrix, 0, 0, 2)
-    C = get_submatrix(matrix, 0, 2, 2)
-    D = get_submatrix(matrix, 2, 0, 2)
-
-    count_even_cols_E = count_greater_than_K(E, K)
-    sum_odd_rows_D = sum(matrix[2]) + sum(matrix[3])
-
-    if count_even_cols_E > sum_odd_rows_D:
-        swap_submatrices(matrix, (0, 0), (2, 0), 1, 1, 2)
-    else:
-        swap_submatrices(matrix, (0, 0), (0, 2), 1, 1, 2)
-
-
-def transpose_matrix(matrix):
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-
+def transpose(matrix):
+    transposed = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    return transposed
 
 def matrix_multiplication(matrix1, matrix2):
     result = []
     for i in range(len(matrix1)):
         row = []
         for j in range(len(matrix2[0])):
-            total = 0
-            for k in range(len(matrix2)):
-                total += matrix1[i][k] * matrix2[k][j]
-            row.append(total)
+            elem = sum(matrix1[i][k] * matrix2[k][j] for k in range(len(matrix2)))
+            row.append(elem)
         result.append(row)
     return result
 
+def scalar_multiplication(matrix, scalar):
+    result = [[matrix[i][j] * scalar for j in range(len(matrix[0]))] for i in range(len(matrix))]
+    return result
 
-def main():
-    K = int(input("Введите K: "))
-    N = int(input("Введите N: "))
+matrix_size = int(input("Введите размер матрицы: "))
 
-    A = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16]
-    ]
-    print("Матрица A:")
-    print_matrix(A)
-    print()
+matrix_a = generate_matrix(matrix_size)
+print("Матрица A:")
+print_matrix(matrix_a)
 
-    calculate_F(A, K)
-    print("Матрица F:")
-    print_matrix(A)
-    print()
+matrix_b = submatrix(matrix_a, 0, matrix_size//2, 0, matrix_size//2)
+matrix_c = submatrix(matrix_a, 0, matrix_size//2, matrix_size//2, matrix_size)
+matrix_d = submatrix(matrix_a, matrix_size//2, matrix_size, 0, matrix_size//2)
+matrix_e = submatrix(matrix_a, matrix_size//2, matrix_size, matrix_size//2, matrix_size)
+print("\nПодматрица B:")
+print_matrix(matrix_b)
+print("\nПодматрица C:")
+print_matrix(matrix_c)
+print("\nПодматрица D:")
+print_matrix(matrix_d)
+print("\nПодматрица E:")
+print_matrix(matrix_e)
 
-    A_transpose = transpose_matrix(A)
-    print("Матрица A транспонированная:")
-    print_matrix(A_transpose)
-    print()
+k_value = 5
+if check_condition(matrix_e, k_value):
+    swap_submatrices(matrix_e, [[0, 0], [1, 0]], [[1, 1], [0, 1]])
+else:
+    swap_submatrices(matrix_b, [[0, 0], [1, 0]], [[1, 1], [0, 1]])
+print("\nМатрица E после преобразования:")
+print_matrix(matrix_e)
 
-    A_times_F = matrix_multiplication(A, A_transpose)
-    print("A * F:")
-    print_matrix(A_times_F)
-    print()
-
-    K_times_A_transpose = [[-K * num for num in row] for row in A_transpose]
-    print("K * Матрицу A транспонированную:")
-    print_matrix(K_times_A_transpose)
-    print()
-
-    result = [[A_times_F[i][j] + K_times_A_transpose[i][j] for j in range(N)] for i in range(N)]
-    print("Результат:")
-    print_matrix(result)
-
-
-main()
+matrix_f_transposed = transpose(matrix_e)
+result = matrix_multiplication(matrix_a, matrix_e)
+k_times_a_transposed = scalar_multiplication(transpose(matrix_a), k_value)
+final_result = [[result[i][j] - k_times_a_transposed[i][j] for j in range(len(result[0]))] for i in range(len(result))]
+print("\nРезультат выражения A*F - K*AT:")
+print_matrix(final_result)
